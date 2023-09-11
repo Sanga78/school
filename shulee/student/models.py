c9de864071839e6495861c11a3cb1d4a510efd77
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-09-11 11:05:39 +0300

    Update models.py
    created the students model

--- shulee/student/models.py
+++ shulee/student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    stu_name= models.CharField(max_length=100)
+    adm_num = models.IntegerField()
+    dob = models.DateField()
+    grade = models.IntegerField()
+    image = models.ImageField
+
+    def __str__(self): 
+            return self.stu_name  
+    
