805c123463838225b427fc211216be8c78688a0a
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-09-18 23:00:31 +0300

    Update views.py
    updated the views

--- shulee/index/views.py
+++ shulee/index/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = UserCreationForm
+
+    def get_success_url(self):
+        return reverse('login')
