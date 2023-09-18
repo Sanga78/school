f37867047a50f4ca35ae9ae9f7c2d32b7d898c05
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-09-18 23:02:19 +0300

    Update urls.py
    updated the urls

--- shulee/student/urls.py
+++ shulee/student/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('login', views.login, name="login"),
-    path('register', views.register, name="register")
+    path('register', views.register, name="register"),
+    path('student/<pk>/',views.stude,name='pupil'),
 ]
