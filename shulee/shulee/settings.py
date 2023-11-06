2c44ebfa33a6c050b7915cc25d2399f1492a85f7
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-06 16:22:45 +0300

    updates
    updates

--- shulee/shulee/settings.py
+++ shulee/shulee/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'shulee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_management',
+        'USER': 'student_management',
+        'PASSWORD': 'student_management_password',
+        'HOST': 'localhost',
+        'PORT': '3306', 
+
     }
 }
 
