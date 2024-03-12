126385dabb3ead760b1c50840676d53672c98897
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-12 14:01:29 +0300

    Update settings.py
    added a default auth model

--- shulee/shulee/settings.py
+++ shulee/shulee/settings.py
@@ -138,4 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = "index.CustomUser"
 LOGIN_REDIRECT_URL = "studen/student/<pk>/"
