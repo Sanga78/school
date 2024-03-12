e12c046cf97c72aaf5a19334ca17e1a73472dc59
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-12 17:03:37 +0300

    Update models.py
    removed username, password, email fields to reduce redundancy

--- shulee/index/models.py
+++ shulee/index/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class CustomUser(AbstractUser):
     user_type_data = ((1,"HOD"),(2,"staff"),(3,"Student"))
@@ -9,9 +10,6 @@ class CustomUser(AbstractUser):
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
@@ -19,9 +17,6 @@ class AdminHOD(models.Model):
 class Staff(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
@@ -46,9 +41,6 @@ class Subjects(models.Model):
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
     gender = models.CharField(max_length=255)
     profile_pic = models.FileField()
     address = models.TextField()
@@ -129,3 +121,23 @@ class NotificaionStudent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects= models.Manager()
+
+#creating signals
+@receiver(post_save,sender=CustomUser)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        if instance.user_type==1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type==2:
+            Staff.objects.create(admin=instance)    
+        if instance.user_type==3:
+            Students.objects.create(admin=instance)
+
+@receiver(post_save,sender=CustomUser)
+def save_user_profile(sender,instance,created,**kwargs):
+    if instance.user_type==1:
+        instance.adminhod.save()
+    if instance.user_type==2:
+        instance.staffs.save()    
+    if instance.user_type==3:
+        instance.students.save()
