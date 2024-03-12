aa4ff5493b0373af66070d5878468bad17969dc1
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-12 13:33:40 +0300

    Update settings.py
    updated the database from mysql to sqlite

--- shulee/shulee/settings.py
+++ shulee/shulee/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'shulee.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_management',
-        'USER': 'student_management',
-        'PASSWORD': 'student_management_password',
-        'HOST': 'localhost',
-        'PORT': '3306', 
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'student_management',
+        # 'USER': 'student_management',
+        # 'PASSWORD': 'student_management_password',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306', 
+  
     }
 }
 
@@ -125,12 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,"static")
+
 STATICFILES_DIRS = [
   'static'
 ]
 MEDIA_URL = "media/"
-MEDIA_ROOT ="media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
