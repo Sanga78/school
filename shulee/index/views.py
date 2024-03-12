fc8ac52e18bdd9a938fa8c67035b459e676efd03
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-12 17:03:01 +0300

    Update views.py
    updated the login view,added logout

--- shulee/index/views.py
+++ shulee/index/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render,redirect,reverse
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import login,logout
+from django.contrib.auth import authenticate
+from index.EmailBackEnd import EmailBackEnd
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-class SignupView(generic.CreateView):
-    template_name = "registration/signup.html"
-    form_class = UserCreationForm
+def loginPage(request):
+    return render(request,'login.html')
 
-    def get_success_url(self):
-        return reverse('login')
+def Login(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed !</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user != None:
+            login(request,user)
+            return HttpResponse("Email : " +request.POST.get("email")+ "Password : "+request.POST.get("password"))
+        else:
+            return HttpResponse("Invalid Login")
+        
+def GetUserDetails(request):
+    if request.user != None:
+        return HttpResponse("User : "+request.user.email+ " usertype : "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+    
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect("/")
