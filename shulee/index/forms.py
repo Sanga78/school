47a7ca95e16bc51ca7a342393d08de3d12407e60
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-23 22:00:07 +0300

    Update forms.py
    updated the courses,fixed an error in the iteration loop

--- shulee/index/forms.py
+++ shulee/index/forms.py
@@ -11,13 +11,15 @@ class AddStudentForm(forms.Form):
     last_name = forms.CharField(label="Last Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     username = forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
-
-    courses = Courses.objects.all()
     course_list = []
-    for course in courses:
-        small_course = (course.id,course.course_name)
-        course_list.append(small_course)
-
+    
+    try:
+        courses = Courses.objects.all()
+        for course in courses:
+            small_course = (course.id,course.course_name)
+            course_list.append(small_course)
+    except:
+        course_list = []
     gender_choices=(
         ("Male","Male"),
         ("Female","Female")
@@ -36,12 +38,14 @@ class EditStudentForm(forms.Form):
     username = forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address = forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    courses = Courses.objects.all()
     course_list = []
-    for course in courses:
-        small_course = (course.id,course.course_name)
-        course_list.append(small_course)
-
+    try:
+        courses = Courses.objects.all()
+        for course in courses:
+            small_course = (course.id,course.course_name)
+            course_list.append(small_course)
+    except:
+        course_list = []
     gender_choices=(
         ("Male","Male"),
         ("Female","Female")
