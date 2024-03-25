4016ae2c2368ea3851cfb7c502569e788057e65b
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-03-25 22:03:55 +0300

    Update models.py
    updated the signals on the sender by updating the session

--- shulee/index/models.py
+++ shulee/index/models.py
@@ -137,7 +137,7 @@ def create_user_profile(sender,instance,created,**kwargs):
         if instance.user_type==2:
             Staffs.objects.create(admin=instance,address="")    
         if instance.user_type==3:
-            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2024-01-01",session_end_year="2026-01-01",address="",profile_pic="",gender="")
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_year_id=SessionYearModel.object.get(id=1),address="",profile_pic="",gender="")
 
 @receiver(post_save,sender=CustomUser)
 def save_user_profile(sender,instance,created,**kwargs):
