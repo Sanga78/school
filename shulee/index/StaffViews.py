5512f7e1bbe126c650961900604978b53a698b0b
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-05-31 19:13:28 +0300

    updates
    updated take attendance

--- shulee/index/StaffViews.py
+++ shulee/index/StaffViews.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse,JsonResponse
+from django.core import serializers
 from django.shortcuts import render
-from index.models import Subjects,SessionYearModel,Students
+from index.models import Subjects,SessionYearModel,Students,Attendance,AttendanceReport
 from django.views.decorators.csrf import csrf_exempt
-
+import json
 def staff_home(request):
     return render(request, "staff_template/staff_home.html")
 
@@ -18,5 +20,34 @@ def get_students(request):
 
     subject = Subjects.objects.get(id=subject_id)
     session_model = SessionYearModel.object.get(id=session_year)
-    students = Students.objects.filter(corse_id=subject.course_id,session_id=session_model)
-    return students
+    students = Students.objects.filter(course_id=subject.course_id,session_year_id=session_model)
+    student_data=serializers.serialize("python",students)
+    list_data=[]
+
+    for student in students:
+        data_small = {"id":student.admin.id,"name":student.admin.first_name+" "+student.admin.last_name}
+        list_data.append(data_small)
+    return JsonResponse(json.dumps(list_data),content_type="application/json",safe=False)
+
+@csrf_exempt
+def save_attendance_data(request):
+    student_ids = request.POST.get("student_ids")
+    subject_id = request.POST.get("subject_id")
+    session_year_id = request.POST.get("session_year_id")
+    attendance_date = request.POST.get("attendance_date")
+    
+    subject_model = Subjects.objects.get(id=subject_id)
+    session_model = SessionYearModel.object.get(id=session_year_id)
+    json_sstudent=json.loads(student_ids)
+    # print(data[0]['id'])
+    try:
+        attendance = Attendance(subject_id=subject_model,attendance_date=attendance_date,session_year_id=session_model)
+        attendance.save()
+
+        for stud in json_sstudent:
+            student = Students.objects.get(admin=stud['id'])
+            attendance_report=AttendanceReport(student_id=student,attendance_id=attendance,status=[stud['status']])
+            attendance_report.save()
+        return HttpResponse("Ok")
+    except:
+        return HttpResponse("Error")
